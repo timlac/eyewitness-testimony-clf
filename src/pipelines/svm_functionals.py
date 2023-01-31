773aa652fef6f28319cefe0c2118b304a47c87af
timlac <tim@lachmann.nu>
2023-01-31 11:55:51 +0100

    dir changes

--- src/pipelines/svm_functionals.py
+++ src/pipelines/svm_functionals.py
@@ -11,7 +11,11 @@ from src.learning.evaluate_scores import evaluate_scores
 from src.learning.parameter_tuning import ParamSearch
 from config import ROOT_DIR, AUDIO_FUNCTIONALS_EGEMAPS_COLS
 
-df = pd.read_csv(os.path.join(ROOT_DIR, "data/opensmile_functionals.csv"), delimiter=";")
+# paths
+input_data = os.path.join(ROOT_DIR, "files/data/opensmile_functionals.csv")
+output_data = os.path.join(ROOT_DIR, "files/out")
+
+df = pd.read_csv(input_data, delimiter=";")
 
 y = df["Accuracy"].values
 x = df[AUDIO_FUNCTIONALS_EGEMAPS_COLS].values
@@ -23,11 +27,13 @@ x = functional_normalize_by(x, participant, method="min_max")
 ps = ParamSearch(mock=True)
 
 clf = ps.param_search(x, y)
-with open("../preprocessing/best_params.json", "w") as outfile:
+
+params_path = os.path.join(output_data, "best_params.json")
+with open(params_path, "w") as outfile:
     # writing to json file
     json.dump(clf.best_params_, outfile)
 
-with open('../preprocessing/best_params.json', 'r') as openfile:
+with open(params_path, 'r') as openfile:
     # Reading from json file
     best_params = json.load(openfile)
 
